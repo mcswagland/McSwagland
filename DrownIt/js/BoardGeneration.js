4517dac6e36c904b4a8c84a08beb53991e92b790
bmcfarland <bmcfarland@harding.edu>
2015-05-12 23:38:15 -0500

    Drown It: Board configs with better moves

--- DrownIt/js/BoardGeneration.js
+++ DrownIt/js/BoardGeneration.js
@@ -10,14 +10,15 @@ function newGameAutomated()
 		
 		//$("#board").empty();
 		pool = [];
-		for(var i = 0; i < BOARD_SIZE; i++)
+		board = convertStringTo2DArray(configs[count].Gameboard);
+		/*for(var i = 0; i < BOARD_SIZE; i++)
 		{
 			for(var j = 0; j < BOARD_SIZE; j++)
 			{
 				board[i][j] = Math.floor(Math.random() * NUM_COLORS);
 				//board[i][j] = convertStringTo2DArray(configs[count].Gameboard)[i][j];
 			}
-		}
+		}*/
 		copyBoardToOriginalBoard();
 		updateBoard();
 		fillColors(board[0][0], {x: 0, y: 0});
