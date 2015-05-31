81ebfa9907e135b29251e5400209ac6d6516b5c0
bmcfarland <bmcfarland@harding.edu>
2015-05-31 00:22:58 -0500

    Added Allure of the Seas cruise cams page

---
+++ CruiseCams/Allure of the Seas/main.js
@@ -0,0 +1,14 @@
+$(window).load(function(){
+	updateCamera();
+	setInterval(updateCamera, 60000);
+})
+
+$('#cameraView').change(function(){
+	updateCamera();
+});
+
+function updateCamera()
+{
+	debugger;
+	$("#picHolder").attr('src',"http://www.royalcaribbean.com/content/shared_assets/webcam/AL/Gallery_" + $('#cameraView').val() + ".jpg" + "?" + new Date().getTime());
+}
