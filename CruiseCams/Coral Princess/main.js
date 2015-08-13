a6e3050bd250fa06155fc9d2efedc7291ff236b6
bmcfarland <bmcfarland@harding.edu>
2015-08-12 23:04:55 -0500

    Site Renovation
    Changed from Pure to Bootstrap CSS. Removed Cruise Cams section from
    site.

--- CruiseCams/Coral Princess/main.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function(){
-	updateCamera();
-	setInterval(updateCamera, 60000);
-})
-
-function updateCamera()
-{
-	$("#picHolder").attr('src',"http://www.princess.com/webcam/coral_bridge.jpg?" + new Date().getTime());
-}
--- CruiseCams/Dawn Princess/main.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function(){
-	updateCamera();
-	setInterval(updateCamera, 60000);
-})
-
-function updateCamera()
-{
-	$("#picHolder").attr('src',"http://www.princess.com/webcam/dawn_bridge.jpg?" + new Date().getTime());
-}
